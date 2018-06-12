0ad59b99735205b04e30928c0874c03787e99c94
Thiago Canto <thiago.canto@rentcars.com>
2018-06-12 11:11:29 -0300

    Changing fetch function to model instead of controller's body

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,5 @@
 class RepositoriesController < ApplicationController
     skip_before_action :verify_authenticity_token
-    require 'net/http'
     
     def index
         @repos = Repository.order(:language)
@@ -9,33 +8,10 @@ class RepositoriesController < ApplicationController
     def search
         languages = ['elixir','ruby','php','javascript','c%23'];
         topRepos = []
+        @response = []
         # params[:languages].each do |language|
         languages.each do |language|
-            url = URI.parse("https://api.github.com/search/repositories?q=language:#{language}&per_page=5")
-            res = Net::HTTP.get_response(url)
-            repos = JSON.parse res.body
-
-            @response = repos
-            puts "Parsing fetched url: #{url}"
-            
-            if repos.to_options[:items] != nil
-                repos.to_options[:items].each do |loadedRepo|
-                    if Repository.where(url:loadedRepo.to_options[:full_name]).take 
-                        puts Repository.where(url:loadedRepo.to_options[:full_name]).take
-                        puts "Repository #{loadedRepo.to_options[:full_name]} found. Skipping insertion."
-                        next
-                    end
-
-                    repo = Repository.new
-                    repo.name = loadedRepo.to_options[:name]
-                    repo.url = loadedRepo.to_options[:full_name]
-                    repo.language = loadedRepo.to_options[:language]
-                    repo.owner = loadedRepo.to_options[:owner].to_options[:login]
-                    repo.description = loadedRepo.to_options[:description]
-                    repo.save
-                end
-            end
-            
+            @response << (Repository.fetch language)
         end
 
         @repos = Repository.order(:language)
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,33 @@
 class Repository < ApplicationRecord
+    require 'net/http'
+
     validates :name, presence: true
     validates :url, presence: true
+
+    def self.fetch( language )
+        url = URI.parse("https://api.github.com/search/repositories?q=language:#{language}&per_page=5")
+        res = Net::HTTP.get_response(url)
+        repos = JSON.parse res.body
+
+        puts "Parsing fetched url: #{url}"
+        if repos.to_options[:items] != nil
+            repos.to_options[:items].each do |loadedRepo|
+                if Repository.where(url:loadedRepo.to_options[:full_name]).take 
+                    puts Repository.where(url:loadedRepo.to_options[:full_name]).take
+                    puts "Repository #{loadedRepo.to_options[:full_name]} found. Skipping insertion."
+                    next
+                end
+
+                repo = Repository.new
+                repo.name = loadedRepo.to_options[:name]
+                repo.url = loadedRepo.to_options[:full_name]
+                repo.language = loadedRepo.to_options[:language]
+                repo.owner = loadedRepo.to_options[:owner].to_options[:login]
+                repo.description = loadedRepo.to_options[:description]
+                repo.save
+            end
+        end
+
+        repos
+    end
 end
